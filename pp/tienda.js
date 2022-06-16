edb8fb558822261fdbcf38a8c350d4d49d774cf0
unknown <shcastillosegura@gmail.com>
2022-06-16 17:39:49 -0500

    tienda2

--- pp/tienda.js
+++ pp/tienda.js
@@ -1,7 +1,28 @@
 
 function descuento(){
-    let nombreCliente = document.getElementById('cliente');
-    let nombreProducto = document.getElementById('producto');
+    let inputCliente = document.getElementById('cliente');
+    let inputProd = document.getElementById('prod1');
+ 
     
-    document.getElementById('cliente_txt').textContent=nombreCliente.value;
+        
+    document.getElementById('cliente_txt').textContent=inputCliente.value;
+    document.getElementById('producto_txt').textContent=inputProd.value;
+    document.getElementById('dscto_porctj').textContent=porcentaje;
+}
+function calcularDscto(){
+    let porcentaje = 0 
+    if (prod1.value =='pan'){
+        porcentaje = 0.955
+    } else if(prod1.value == 'carne'){
+        porcentaje = 0.985
+    }
+    let inputPrecio = document.getElementById('precio1');
+    let precio1 = inputPrecio.value
+    let cantidadProd = document.getElementById('cantidad');
+
+    let quant1 = cantidadProd.value
+
+    let resultadoPrecio=precio1*porcentaje*quant1;
+    
+    document.getElementById('costo_total').textContent=resultadoPrecio.value;
 }
